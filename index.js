c09f5d49082ed02a4c4fd7bdef7440fcfe0652a6
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-27 21:17:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function makeCake() {
 
 function makePie() {
   var updatePieStatus;
-  mix(updatePieStatus)
+  pie.decorate = cake.decorate.bind(pie);
+  mix.call(pie, updatePieStatus)
 }
 
 function updateStatus(statusText) {
